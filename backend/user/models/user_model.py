827aad4e6ca96d9972af5296247f08bfd7b151ab
michael <michaeldo495@gmail.com>
2025-05-30 23:51:21 -0700

    add user account db model

--- backend/user/models/user_model.py
+++ backend/user/models/user_model.py
@@ -1,10 +1,16 @@
-from sqlalchemy.orm import mapped_column
-
-from pydantic import BaseModel
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, DateTime, UUID
+from datetime import datetime, timezone 
+from core.database import Base
 from uuid import UUID
 
 
-class User(BaseModel):
-    id:UUID
-    username:str
+class User(Base):
+    __tablename__ = "users"
+    id: Mapped[UUID] = mapped_column(UUID, primary_key=True)
+    username:Mapped[str] = mapped_column(String(20), unique= True, nullable=False)
+    email: Mapped[str] = mapped_column(String (64),unique=True,nullable=False)
+    created_at: Mapped[DateTime] = mapped_column(DateTime, default=datetime.now(timezone.utc), nullable=False)
+    updated_at: Mapped[DateTime] = mapped_column(DateTime, default=datetime.now(timezone.utc), onupdate=datetime.now(timezone.utc))
+    
     
